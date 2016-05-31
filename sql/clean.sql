cdae2a10622f6a0305ea8e1bd78917f3355a970f
Joseph Arhar <josepharhar@gmail.com>
2016-05-30 22:59:28 -0700

    Added sql files

---
+++ sql/clean.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS CharacterAnimationCommands;
+DROP TABLE IF EXISTS AnimationCommandTypes;
+DROP TABLE IF EXISTS SharedAnimations;
+DROP TABLE IF EXISTS CharacterAttributes;
+DROP TABLE IF EXISTS Characters;
