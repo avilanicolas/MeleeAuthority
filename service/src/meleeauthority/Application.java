364af88e63121cda37965958151c6198e4b8ed92
Nicolas Avila <avilanicolasc@gmail.com>
2016-06-07 18:47:02 -0700

    mysql in java

---
+++ service/src/meleeauthority/Application.java
@@ -0,0 +1,22 @@
+package meleeauthority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            new ClassPathXmlApplicationContext("beans.xml");
+
+        MeleeDB meleeDB =
+            (MeleeDB) context.getBean("meleeJDBCTemplate");
+
+        meleeDB.asdf();
+
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ service/src/meleeauthority/MeleeDAO.java
@@ -0,0 +1,9 @@
+package meleeauthority;
+
+import javax.sql.DataSource;
+
+public interface MeleeDAO {
+    public void setDataSource(DataSource ds);
+
+    public void asdf();
+}
