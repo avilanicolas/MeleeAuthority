5ba4fbe6c6af146a837a97ea3edac394d6d775e8
t3chn0manc3r <agillis.tech@gmail.com>
2016-06-10 13:11:28 -0700

    Update BiggestDisjoint.sql

--- service/sql/BiggestDisjoint.sql
+++ service/sql/BiggestDisjoint.sql
@@ -1,7 +1,8 @@
 -- Find attack with largest disjoint
-SELECT C.fullName, animation,
+SELECT C.fullName, SA.description,
    MAX(SQRT(POW(xoffset,2) + POW(yoffset,2) + POW(zoffset,2))) AS disjoint
 FROM Hitboxes
    JOIN Characters C ON C.id = charId
+   JOIN SharedAnimations SA ON SA.internalName = animation
 GROUP BY charId, animation
 ORDER BY MAX(SQRT(POW(xoffset,2) + POW(yoffset,2) + POW(zoffset,2))) DESC, C.fullName;
