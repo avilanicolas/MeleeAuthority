c88bebf4268ab35617788438bc83563f957d771f
t3chn0manc3r <agillis.tech@gmail.com>
2016-06-10 13:09:49 -0700

    AttackNames

--- service/sql/QuickestHit.sql
+++ service/sql/QuickestHit.sql
@@ -1,7 +1,8 @@
 -- Find quickest hitbox
-SELECT fullName, animation, MIN(frame) AS earliestAttackFrame
+SELECT fullName, SA.description, MIN(frame) AS earliestAttackFrame
 FROM FrameStrips
    JOIN Characters C ON C.id = charId
+   JOIN SharedAnimations SA.internalName = animation
 WHERE hitbox = true
 GROUP BY charId, animation
 ORDER BY MIN(frame) ASC;
