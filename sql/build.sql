cf8ef05872fe50257365699bc2648e2ee7b1da53
Nicolas Avila <avilanicolasc@gmail.com>
2016-06-10 00:05:13 -0400

    put sql code in shared location

--- sql/build.sql
+++
@@ -1,7 +0,0 @@
-source Characters.sql
-source CharacterAttributes.sql
-source SharedAnimations.sql
-source AnimationCommandTypes.sql
-source CharacterAnimationCommands.sql
-source FrameStrips.sql
-source Hitboxes.sql
--- sql/clean.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS Hitboxes;
-DROP TABLE IF EXISTS FrameStrips;
-DROP TABLE IF EXISTS CharacterAnimationCommands;
-DROP TABLE IF EXISTS AnimationCommandTypes;
-DROP TABLE IF EXISTS SharedAnimations;
-DROP TABLE IF EXISTS CharacterAttributes;
-DROP TABLE IF EXISTS Characters;
