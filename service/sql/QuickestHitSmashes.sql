66ca6697112b63a3430ff88d38fd6b652a8f3e35
Joseph Arhar <josepharhar@gmail.com>
2016-06-10 20:35:46 -0700

    added quickest smashes

--- service/sql/QuickestHitSmashes.sql
+++ service/sql/QuickestHitSmashes.sql
@@ -4,6 +4,11 @@ FROM FrameStrips F
    JOIN Characters C ON C.id = charId
    JOIN SharedAnimations SA ON SA.internalName = animation
 WHERE hitbox = true
+    AND (SA.internalName = 'AttackLw4'
+        OR SA.internalName = 'AttackS4S'
+        OR SA.internalName = 'AttackS4Hi'
+        OR SA.internalName = 'AttackS4Lw'
+        OR SA.internalName = 'AttackHi4')
 GROUP BY charId, animation
 ORDER BY MIN(frame) ASC
 LIMIT 20;
