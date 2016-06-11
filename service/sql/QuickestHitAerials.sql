b8300eeb1553a666c81373a4c1191a005902c2da
Joseph Arhar <josepharhar@gmail.com>
2016-06-10 22:55:47 -0700

    analysis query per character

--- service/sql/QuickestHitAerials.sql
+++ service/sql/QuickestHitAerials.sql
@@ -12,3 +12,48 @@ WHERE hitbox = true
 GROUP BY charId, animation
 ORDER BY MIN(frame) ASC
 LIMIT 20;
+
+-- quickest hitbox per character
+SELECT C.fullName AS 'character', SA.description AS move, SS1.earliestFrame
+FROM (
+        SELECT charId, animation, MIN(frame) AS earliestFrame
+        FROM FrameStrips
+        WHERE
+            hitbox = TRUE
+            AND (animation = 'AttackAirF'
+                OR animation = 'AttackAirHi'
+                OR animation = 'AttackAirLw'
+                OR animation = 'AttackAirB'
+                OR animation = 'AttackAirN')
+        GROUP BY charId, animation
+    ) SS1 LEFT JOIN (
+        -- loser aerials per character
+        SELECT S1.charId, S1.animation
+        FROM (
+                SELECT charId, animation, MIN(frame) AS earliestFrame
+                FROM FrameStrips
+                WHERE
+                    hitbox = TRUE
+                    AND (animation = 'AttackAirF'
+                        OR animation = 'AttackAirHi'
+                        OR animation = 'AttackAirLw'
+                        OR animation = 'AttackAirB'
+                        OR animation = 'AttackAirN')
+                GROUP BY charId, animation
+            ) S1 JOIN (
+                SELECT charId, animation, MIN(frame) AS earliestFrame
+                FROM FrameStrips
+                WHERE
+                    hitbox = TRUE
+                    AND (animation = 'AttackAirF'
+                        OR animation = 'AttackAirHi'
+                        OR animation = 'AttackAirLw'
+                        OR animation = 'AttackAirB'
+                        OR animation = 'AttackAirN')
+                GROUP BY charId, animation
+            ) S2 ON S1.charId = S2.charId AND S1.earliestFrame > S2.earliestFrame
+    ) SS2 ON SS1.animation = SS2.animation AND SS1.charId = SS2.charId
+    JOIN Characters C ON C.id = SS1.charId
+    JOIN SharedAnimations SA ON SA.internalName = SS1.animation
+WHERE SS2.animation IS NULL
+ORDER BY earliestFrame, 'character';
